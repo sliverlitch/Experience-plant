b4b650deabd95f55ea7735f0ab59a55b016f4479
sliverlitch <155404487+sliverlitch@users.noreply.github.com>
2024-05-12 14:27:38 -1000

    Update mainmod.java

--- mainmod.java
+++ mainmod.java
@@ -3,16 +3,16 @@ package com.barrow.revelations;
 import com.barrow.revelations.blocks.ExperiencePlantBlock;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class MainMod implements ModInitializer {
-    public static final Block INSIGHT_SPROUT_1 = new ExperiencePlantBlock(100);
-    public static final Block INSIGHT_BUSH_2 = new ExperiencePlantBlock(200);
-    public static final Block EPIPHANY_SPRIG_3 = new ExperiencePlantBlock(300);
-    public static final Block REVELATION_FLOWER_4 = new ExperiencePlantBlock(400);
+
+    public static final Block INSIGHT_SPROUT_1 = new ExperiencePlantBlock(100, ParticleTypes.ASH);
+    public static final Block INSIGHT_BUSH_2 = new ExperiencePlantBlock(200, ParticleTypes.WARPED_SPORE);
+    public static final Block EPIPHANY_SPRIG_3 = new ExperiencePlantBlock(300, ParticleTypes.ENCHANT);
+    public static final Block REVELATION_FLOWER_4 = new ExperiencePlantBlock(400, ParticleTypes.NAUTILUS);
 
     @Override
     public void onInitialize() {
@@ -22,7 +22,7 @@ public class MainMod implements ModInitializer {
         registerBlock(REVELATION_FLOWER_4, "revelation_flower_4");
     }
 
-        private void registerBlock(Block block, String name) {
+    private void registerBlock(Block block, String name) {
         Registry.register(Registry.BLOCK, new Identifier("barrows_revelations", name), block);
         Registry.register(Registry.ITEM, new Identifier("barrows_revelations", name), new BlockItem(block, new Item.Settings()));
     }
