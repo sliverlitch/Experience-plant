cb70cf35f5e8246f4ff631db6b0b337f033eb134
sliverlitch <155404487+sliverlitch@users.noreply.github.com>
2024-05-12 14:00:28 -1000

    Update mainmod.java

--- mainmod.java
+++ mainmod.java
@@ -1,6 +1,6 @@
-package com.example.mod;
+package com.barrow.revelations;
 
-import com.example.mod.blocks.ExperiencePlantBlock;
+import com.barrow.revelations.blocks.ExperiencePlantBlock;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
@@ -9,7 +9,6 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class MainMod implements ModInitializer {
-
     public static final Block INSIGHT_SPROUT_1 = new ExperiencePlantBlock(100);
     public static final Block INSIGHT_BUSH_2 = new ExperiencePlantBlock(200);
     public static final Block EPIPHANY_SPRIG_3 = new ExperiencePlantBlock(300);
@@ -23,8 +22,8 @@ public class MainMod implements ModInitializer {
         registerBlock(REVELATION_FLOWER_4, "revelation_flower_4");
     }
 
-    private void registerBlock(Block block, String name) {
-        Registry.register(Registry.BLOCK, new Identifier("examplemod", name), block);
-        Registry.register(Registry.ITEM, new Identifier("examplemod", name), new BlockItem(block, new Item.Settings()));
+        private void registerBlock(Block block, String name) {
+        Registry.register(Registry.BLOCK, new Identifier("barrows_revelations", name), block);
+        Registry.register(Registry.ITEM, new Identifier("barrows_revelations", name), new BlockItem(block, new Item.Settings()));
     }
 }
